9abd2ac6f4a93580215d5b743807f2cfece4558c
franm <fgtenaglia96@gmail.com>
2023-01-10 15:59:42 -0300

    nuevo cambio

--- js/formulario.js
+++ js/formulario.js
@@ -1,62 +1,23 @@
+let today = new Date();
 let btnRegistrar=document.getElementById('RegistrarTurno')
 
-btnRegistrar.onclick = (event)=>{
-    event.preventDefault()
-    alert('Ya reservaste el turno')
-};
-/*
+btnRegistrar.onclick = (event) => {
+    let nombre = document.getElementById("inputNombre").value
+    let apellido = document.getElementById("inputApellido").value
+    let id = document.getElementById("inputID").value
+    let area = document.getElementById("area").value
+    let turno = document.getElementById("turno").value
 
-let nombre = prompt("Ingrese nombre");
-let apellido = prompt("Ingresa el apellido");
-let documento= prompt("Ingrese su numero de documento");
-*/
-
-/*
-class Personita {
-    constructor(nombrePaciente, apellidoPaciente, documentoPaciente) {
-        this.nombreSession = nombrePaciente  ;
-        this.apellidoSession = apellidoPaciente ;
-        this.documentoSession = documentoPaciente;
+    let usuario = {
+        nombre: nombre,
+        apellido: apellido,
+        id: id,
+        area: area,
+        turno: turno, 
     }
+
+    localStorage.setItem("user", JSON.stringify(usuario))
+    event.preventDefault()
+    alert('Ya reservaste el turno con el ID:'+ id +' ' +today )
+
 };
-
-*/
-
-//let paciente= new Personita(nombre,apellido,documento);
-
-//Aca obtengo el listado de pacientes registrados
-
-//const pacienteRegistrado = JSON.parse(localStorage.getItem("listaPacientes"));
-//const paciente=[];
-
-
-
-//Ahora itero pacienteRegistrado con for ..of para transformar todos sus objetos a tipo paciente
-/*for(const objeto of pacienteRegistrado)
-    paciente.push(new personita(objeto) );
-//Ahora tengo objeto pacientes y puedo usarle los metodos
-
-for (const Paciente of personita)
-    console.log(personitas);
-*/
-
-
-/*
-localStorage.setItem('nombre', 'fran');
-
-
-sessionStorage.setItem('Nombre','Francis');
-
-
-sessionStorage.setItem('Nombre',nombreSession);
-sessionStorage.setItem('Apellido',apellidoSession);
-sessionStorage.setItem('Documento',documentoSession);
-
-*/
-
-let mibotonsito = document.getElementById("mybutton");
-mybutton.addEventListener("click",probandoFuncion);
-
-function probandoFuncion(){
-  alert("anduvo");
-}
